bdc8190a9ec4c924a967b0bc7d75e1fbfcf870b6
shireneshou <shirene.shoushtarizadeh@kellogg.northwestern.edu>
2023-02-21 06:01:07 +0000

    adding in completed coded

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -1,3 +1,11 @@
 class DiceController < ApplicationController
 
-end
+    def index
+      @die1 = rand(1..6)
+      @die2 = rand(1..6)
+      @total = @die1 + @die2
+      @name = params["name"]
+    end
+  
+  end
+  
